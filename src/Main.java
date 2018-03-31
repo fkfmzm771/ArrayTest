8b58356b5404d5ef12fe2723681d47affd9ca498
DESKTOP-PSB5I22\fkfmz <fkfmzm77@naver.com>
2018-04-01 00:42:45 +0900

    컨티뉴 실습

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
-public class Main {
+public class Main extends ArrayList<Integer> {
 	public static void main(String[] args) {
+
+		Main m = new Main();
+
+		for (int i = 0; i < 100 ; i++) {
+			m.add(i+1);
+		}
+
+
+		Iterator<Integer> iter_m = m.iterator();
+
+		do {
+			int a = iter_m.next();
+			if (a == 10){
+				break;
+			}
+			System.out.println(a);
+		}while (iter_m.hasNext());
+
 
 	}
 }
